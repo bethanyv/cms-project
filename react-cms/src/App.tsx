7aed281253da8edb0a8ef14a32cfe1173f3ae765
Bethany <Bethanyv21@gmail.com>
2022-04-05 22:15:02 -0500

    cleaned up

--- react-cms/src/App.tsx
+++ react-cms/src/App.tsx
@@ -2,17 +2,6 @@ import './App.css';
 import TripsComponent from './components/TripsComponent';
 
 function App() {
-  // let loadedTrips: { place: string, startDate: string }[] = [];
-
-  // const renderTrips = () => {
-  //   let tripList: string[] = [];
-  //   loadedTrips.map(trip => {
-  //       return tripList.push(`<div key=${trip.place}>You're going to ${trip.place} on ${trip.startDate}!</div>`)
-  //   })
-  //   console.log(tripList)
-  //   return tripList;
-  // };
-
   return (
     <div className="App">
       <header className="App-header">
